4b3e43fdf9b003f52fe9f33f9ab68a61be1de84d
SRIRAM-POTHUMANI <84328532+SRIRAM-POTHUMANI@users.noreply.github.com>
2021-12-11 02:58:59 +0530

    commit

--- src/Chat.js
+++ src/Chat.js
@@ -120,7 +120,7 @@ export default function Chat() {
   };
   useEffect(() => {
     // userlist();
-    sync();
+    // sync();
     scrollToBottom();
   });
 
--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://rps-msg.herokuapp.com",
+  baseURL: "http://localhost:5000",
 });
 export default instance;
