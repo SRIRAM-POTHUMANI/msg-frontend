10b8e1155b92472b98de2d0048dd7bd91f9d3233
SRIRAM-POTHUMANI <84328532+SRIRAM-POTHUMANI@users.noreply.github.com>
2021-12-11 02:33:05 +0530

    axios

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000",
+  baseURL: "https://rps-msg.herokuapp.com/",
 });
-export default instance
+export default instance;
